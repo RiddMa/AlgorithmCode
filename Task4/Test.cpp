96dfce214ac9e592f5c80375e0a1d3ce28c35667
RiddMa <riddma@qq.com>
2020-10-08 18:12:01 +0800

    2020年10月8日18:11:40 更新round robin问题解法

---
+++ Task4/Test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Ridd on 2020/10/08/008.
+//
+
+#include "Test.h"
+
+int main() {
+    int playerNum(5);
+    int duration = playerNum % 2 == 0 ? (playerNum - 1) : playerNum;
+
+
+    return 0;
+}
---
+++ Task4/Test.h
@@ -0,0 +1,19 @@
+//
+// Created by Ridd on 2020/10/08/008.
+//
+
+#ifndef FINALTEST_TEST_H
+#define FINALTEST_TEST_H
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <functional>
+#include <climits>
+
+#include "RoundRobin.h"
+
+#endif //FINALTEST_TEST_H
